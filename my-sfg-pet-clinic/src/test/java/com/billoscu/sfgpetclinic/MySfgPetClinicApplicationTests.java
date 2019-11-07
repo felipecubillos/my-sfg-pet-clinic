0810728f7e9015581023ed1bb54eeb36356c2041
Felipe Cubillos <felipe.cubillos@outlook.com>
2019-11-06 22:23:15 -0500

    First Commmit

---
+++ my-sfg-pet-clinic/src/test/java/com/billoscu/sfgpetclinic/MySfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billoscu.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
