90b54a935fb14aeadfb5eeebbe42b71260a52a41
Felipe Cubillos <felipe.cubillos@outlook.com>
2020-03-03 22:26:41 -0500

    refactor abstractMapService to generate ID . closes #23

--- my-sfg-pet-clinic/pet-clinic-web/src/main/java/com/billoscu/sfgpetclinic/bootstrap/DataLoader.java
+++ my-sfg-pet-clinic/pet-clinic-web/src/main/java/com/billoscu/sfgpetclinic/bootstrap/DataLoader.java
@@ -8,59 +8,51 @@ import com.billoscu.sfgpetclinic.model.Vet;
 import com.billoscu.sfgpetclinic.services.OwnerService;
 import com.billoscu.sfgpetclinic.services.VetService;
 
-
-
 @Component
-public class DataLoader implements CommandLineRunner{
-	
-	
- private final OwnerService ownerService;	
- private final VetService vetService;
- 
-	
+public class DataLoader implements CommandLineRunner {
+
+	private final OwnerService ownerService;
+	private final VetService vetService;
+
 	public DataLoader(OwnerService ownerService, VetService vetService) {
-	this.ownerService = ownerService;
-	this.vetService = vetService;
-}
-
-
+		this.ownerService = ownerService;
+		this.vetService = vetService;
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
+
 		owner1.setFirstName("Andres");
 		owner1.setLastName("Cubillos");
-		
+
 		ownerService.save(owner1);
-		
+
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
+
 		owner2.setFirstName("Felipe");
 		owner2.setLastName("Estrada");
-		
+
 		ownerService.save(owner2);
 		System.out.println("Created Owners.....");
-		
+
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
+
 		vet1.setFirstName("LJF");
 		vet1.setLastName("Franco");
-		
+
 		vetService.save(vet1);
-		
+
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
+
 		vet2.setFirstName("LJF");
 		vet2.setLastName("Gonzalez");
-		
+
 		vetService.save(vet2);
-		
+
 		System.out.println("Created Vets.....");
-		
+
 	}
 
-	
-	
 }
